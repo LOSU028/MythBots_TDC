440a1a57da7b29b3b4f3ef6a84a43e0ff02b7da7
LOSU028 <soflu021@outlook.com>
2024-09-30 19:42:22 -0600

    Ruteo de la app, implementacion de estilos para contacto y configuracion de perfil, ademas se agregaron componentes de home y dashboard asi como su ruteo

--- MythBots/src/app/app.routes.ts
+++ MythBots/src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
 import { ConfigPerfilComponent } from './components/paths/config-perfil/config-perfil.component';
 import { ContactanosComponent } from './components/paths/contactanos/contactanos.component';
+import { LoginComponent } from './components/paths/login/login.component';
+import { RegistrarseComponent } from './components/paths/registrarse/registrarse.component';
+import { HomeComponent } from './components/paths/home/home.component';
+import { DashboardComponent } from './components/paths/dashboard/dashboard.component';
 export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch:'full'},
+    {path: 'login', component:LoginComponent},
+    {path: 'register',  component: RegistrarseComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'dashboard', component:DashboardComponent},
     {path: 'contact', component:ContactanosComponent},
     {path: 'config', component:ConfigPerfilComponent}
 ];
--- MythBots/src/app/components/paths/config-perfil/config-perfil.component.ts
+++ MythBots/src/app/components/paths/config-perfil/config-perfil.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-config-perfil',
-  standalone: true,
-  imports: [],
   templateUrl: './config-perfil.component.html',
-  styleUrl: './config-perfil.component.scss'
+  styleUrls: ['./config-perfil.component.scss'],
+  standalone: true,
+  imports: [FormsModule, CommonModule]
 })
 export class ConfigPerfilComponent {
+  activeTab: string = 'perfil'; user = {
+    nombre: '',
+    apellidos: '',
+    correo: '',
+    telefono: '',
+    direccion: '',
+    ciudad: '',
+    estado: '',
+    cp: ''
+  };
 
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
+
+  reset() {
+    if(confirm("¿Esta seguro de que desea eliminar sus cambios?")) {
+      console.log("Implementar pop up de confirmacion");
+    }
+  }
+
+  save() {
+    
+  }
 }
