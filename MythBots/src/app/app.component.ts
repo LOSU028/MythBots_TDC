02df5031442e9c558a49bd8cdbe87fd75e05b7e0
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-10-06 13:51:03 -0600

    Implementacion del navbar 80%

--- MythBots/src/app/app.component.ts
+++ MythBots/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- MythBots/src/app/components/layout/navbar/navbar.component.ts
+++ MythBots/src/app/components/layout/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+  // Definición de los elementos del menú
+  menuItems: string[] = [
+    '3D Print',
+    'PCB Design',
+    'Create Funko',
+    'Courses',
+    'Educational Robotics',
+    'Contact'
+  ];
 }
