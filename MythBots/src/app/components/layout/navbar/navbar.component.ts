4a322435989c7546abaf2330214d3686a3f98b68
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-10-13 15:43:32 -0600

    Implementacion del Navbar, con responsabilidad y en espera de creacion de sidebar 80%

--- MythBots/src/app/components/layout/navbar/navbar.component.ts
+++ MythBots/src/app/components/layout/navbar/navbar.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
   // Definición de los elementos del menú
-  menuItems: string[] = [
-    '3D Print',
-    'PCB Design',
-    'Create Funko',
-    'Courses',
-    'Educational Robotics',
-    'Contact'
+  menuItems = [
+    { title: '3D Print', path: '/3d-print' },
+    { title: 'PCB Desing', path: '/pcb-desing' },
+    { title: 'Create Funko', path: '/create-funko' },
+    { title: 'Courses', path: '/courses' },
+    { title: 'Educational Robotics', path: '/educational-robotics' },
+    { title: 'Contact', path: '/contact' },
   ];
+
+  // Acción al hacer clic en el icono de hamburguesa
+  onHamburgerClick(): void {
+    console.log('Hamburguesa clickeada'); // Simulación del sidebar
+  }
 }
