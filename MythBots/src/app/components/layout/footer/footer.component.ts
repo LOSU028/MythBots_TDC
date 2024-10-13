6ba749c4e7dab587d481a15e5abbed6320ccccad
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-10-12 21:00:41 -0600

    Añadiendo iconos, Modificando estilos y añadiendo, implementando mas ajustes dinamicos.

--- MythBots/src/app/components/layout/footer/footer.component.ts
+++ MythBots/src/app/components/layout/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
 
@@ -23,28 +24,15 @@ export class FooterComponent {
 
   //Apartado de Contactos
   contacts = [
-    { icon: '📞', text: '+52 123 123 123' },
-    { icon: '📧', text: 'correogmail.com' }
+    { icon: 'fab fa-whatsapp', text: '+52 123 123 123' },
+    { icon: 'fas fa-envelope', text: 'correogmail.com' }
   ];
 
   //Apartado de Redes sociales
   socialLinks = [
-    {
-      icon: '../../../../../public/resources/facebook.png',
-      url: 'https://www.facebook.com'
-    },
-    {
-      icon: '../../../../../public/resources/twitter.png',
-      url: 'https://www.twitter.com'
-    },
-    {
-      icon: '../../../../../public/resources/instagram.png',
-      url: 'https://www.instagram.com'
-    },
-    {
-      icon: '../../../../../public/resources/linkedin.png',
-      url: 'https://www.linkedin.com'
-    }
+    { name: 'LinkedIn', icon: 'fab fa-linkedin', url: 'https://www.linkedin.com', color: '#0077B5' },
+    { name: 'Instagram', icon: 'fab fa-instagram', url: 'https://www.instagram.com', color: '#C13584' },
+    { name: 'Discord', icon: 'fab fa-discord', url: 'https://discord.com', color: '#7289DA' }
   ];
 
 
