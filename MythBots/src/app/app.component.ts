c9211b39e6a5c8940712378d05602bd3ee92fb9b
LOSU028 <soflu021@outlook.com>
2024-10-15 13:21:58 -0600

    implementacion de formulario de registro, sin estilos, con validaciones y redireccionamiento a pagina de login si es que ya se tiene una cuenta

--- MythBots/src/app/app.component.ts
+++ MythBots/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
-@Component({
-  selector: 'app-root',
+@Component({  selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
--- MythBots/src/app/components/paths/registrarse/registrarse.component.ts
+++ MythBots/src/app/components/paths/registrarse/registrarse.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registrarse',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, RouterLink, ],
   templateUrl: './registrarse.component.html',
   styleUrl: './registrarse.component.scss'
 })
 export class RegistrarseComponent {
+  form: FormGroup;
 
+  constructor(formBuilder: FormBuilder){
+    this.form = formBuilder.group({
+      username:['', [Validators.required, Validators.minLength(8)]],
+      email: ['',[Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern('((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,30})')]]
+    })
+  }
+
+  registrar(){
+    if (this.form.valid){
+      console.log('Registrando usuario...')
+    }
+  }
 }
