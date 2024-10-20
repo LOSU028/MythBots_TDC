3dbfdb15f2a8d61f7bb79c2a2170c5349a1b7256
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-19 23:03:43 -0600

    Implementacion de dinamico en componente card-cuenta. y campo de admin.

--- MythBots/src/app/components/layout/card-cuenta/card-cuenta.component.ts
+++ MythBots/src/app/components/layout/card-cuenta/card-cuenta.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-cuenta',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-cuenta.component.html',
   styleUrl: './card-cuenta.component.scss'
 })
 export class CardCuentaComponent {
 
+  //Datos del usuario
+  usuario = {
+    nombre: 'Nombre usuario',
+    fotoPerfil: 'resources/mythbots_logo.png'
+  };
+
+  // Opciones del menú
+  admin = { icono: 'fa-solid fa-shield' ,texto:'Panel Administracion'};
+
+  // Opciones del menú
+  opciones = [
+    { icono: 'fa-solid fa-user', texto: 'Perfil' },
+    { icono: 'fa-solid fa-book', texto: 'Mis Cursos' },
+    { icono: 'fa-solid fa-key', texto: 'Mis Licencias' }
+  ];
+
+  //opcin de cerrar sesion
+  cerrarSesion = { icono: 'fa-solid fa-right-from-bracket', texto: 'Cerrar Sesión' };
+
+  //Simulacion del rol admin (Un futuro se implementara middleware)
+  isAdmin = false;
 }
