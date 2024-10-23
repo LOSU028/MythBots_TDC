69f8c074539c98b44878bc5bc47256a8867672d6
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-23 16:10:38 -0600

    Pequeños ajustes y comentarios.

--- MythBots/src/app/app.routes.ts
+++ MythBots/src/app/app.routes.ts
@@ -6,12 +6,28 @@ import { RegistrarseComponent } from './components/paths/registrarse/registrarse
 import { HomeComponent } from './components/paths/home/home.component';
 import { CreateFunkoComponent } from './components/paths/create-funko/create-funko.component';
 import { NgModule } from '@angular/core';
+import { CustomizeFunkoComponent } from './components/paths/create-funko/customize-funko/customize-funko.component';
 export const routes: Routes = [
+
+    // default
     { path: '', redirectTo: 'home', pathMatch: 'full' },
+    // /login
     { path: 'login', component: LoginComponent },
+    // /register
     { path: 'register', component: RegistrarseComponent },
+    // /home
     { path: 'home', component: HomeComponent },
-    { path: 'create-funko', component: CreateFunkoComponent },
+    {
+        //Ruta /create-funko
+        path: 'create-funko', component: CreateFunkoComponent, children: [
+            // /create-funko/customize
+            { path: 'customize', component: CustomizeFunkoComponent }
+        ]
+    },
+    // /contact
     { path: 'contact', component: ContactanosComponent },
+    // /config
     { path: 'config', component: ConfigPerfilComponent },
+    //Ruta comodin
+    //{ path: '**', component: PageNotFoundComponent}
 ];
