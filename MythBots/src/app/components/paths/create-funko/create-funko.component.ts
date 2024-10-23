e59edcb7143630e83c7b74bd93a63cb90fc00832
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-22 22:32:14 -0600

    Creacion de nuevo componente  "create-funko"

---
+++ MythBots/src/app/components/paths/create-funko/create-funko.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-funko',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-funko.component.html',
+  styleUrl: './create-funko.component.scss'
+})
+export class CreateFunkoComponent {
+
+}
