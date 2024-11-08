334b0e83819851d87f9f8522224a686531f18670
VMLAP\VMLap <victor.telles@iteso.mx>
2024-11-08 13:41:21 -0600

    PCBDesing, Dinamico.

--- MythBots/src/app/components/paths/pcb-desing/pcb-desing.component.ts
+++ MythBots/src/app/components/paths/pcb-desing/pcb-desing.component.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
 import { PcbBasicComponent } from './pcb-basic/pcb-basic.component';
 import { PcbCustomizeComponent } from './pcb-customize/pcb-customize.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pcb-desing',
   standalone: true,
-  imports: [PcbBasicComponent, PcbCustomizeComponent],
+  imports: [PcbBasicComponent, PcbCustomizeComponent, CommonModule],
   templateUrl: './pcb-desing.component.html',
   styleUrl: './pcb-desing.component.scss'
 })
 export class PcbDesingComponent {
 
+  // Banner
+  bannerTitle: string = 'PCB Design';
+  bannerDescription: string = 'Agregar una descripción del servicio de diseño de PCB.';
+  bannerImageUrl: string = 'path/to/image.jpg';
+
+  // Opciones de paquete
+  paquetes = [
+    { id: 'basico', titulo: 'Básico' },
+    { id: 'personalizado', titulo: 'Personalizado' }
+  ];
+
+  // Campos del formulario
+  camposFormulario = [
+    { id: 'nombre', titulo: 'Nombre(s)', type: 'text', placeholder: 'Escribe tu nombre', requerido: true },
+    { id: 'apellido', titulo: 'Apellido(s)', type: 'text', placeholder: 'Escribe tu apellido', requerido: true },
+    { id: 'email', titulo: 'Email', type: 'email', placeholder: 'Escribe tu correo electronico', requerido: true },
+    { id: 'phone', titulo: 'Telefono', type: 'number', placeholder: 'Escribe tu numero telefonico', requerido: true }
+  ];
+
+  archivos: { id: string }[] = [{ id: 'archivo1' }];
+
+  // Añadir un archivo al formulario
+  addArchivo() {
+    const nuevoArchivo = { id: `archivo${this.archivos.length + 1}` };
+    this.archivos.push(nuevoArchivo);
+  }
+
+  // Eliminar un archivo del formulario
+  deleteArchivo(index: number) {
+    this.archivos.splice(index, 1);
+  }
+
+  // Manejo del envío del formulario
+  enviarFormulario() {
+    console.log("Formulario enviado.");
+  }
 }
